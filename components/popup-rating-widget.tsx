badf57fc678b0ee1e7c741fb85e0d6435e8de47f
Guardx12 <Guardx.alerts@gmail.com>
2026-02-03 12:57:47 +0000

    Add files via upload

--- components/popup-rating-widget.tsx
+++ components/popup-rating-widget.tsx
@@ -1,17 +1,35 @@
 "use client"
 
-import Script from "next/script"
+import { useEffect } from "react"
 
 export function PopupRatingWidget() {
-  return (
-    <div id="popup-rating-widget">
-      <Script
-        id="popup-rating-widget-script"
-        src="https://widget.reviewability.com/js/popupWidget.min.js"
-        data-gfspw="https://guardx.reviewability.com/popup-pixel/get/45047e1ca219820a0f147c3dfc8636f15ef9a234"
-        strategy="afterInteractive"
-        async
-      />
-    </div>
-  )
+  useEffect(() => {
+    // Guard against duplicate script loads
+    if (document.getElementById("popup-rating-widget-script")) {
+      return
+    }
+
+    const script = document.createElement("script")
+    script.id = "popup-rating-widget-script"
+    script.src = "https://widget.reviewability.com/js/popupWidget.min.js"
+    script.setAttribute(
+      "data-gfspw",
+      "https://guardx.reviewability.com/popup-pixel/get/45047e1ca219820a0f147c3dfc8636f15ef9a234"
+    )
+    script.async = true
+
+    document.body.appendChild(script)
+
+    return () => {
+      // Cleanup on unmount
+      const existingScript = document.getElementById("popup-rating-widget-script")
+      if (existingScript) {
+        existingScript.remove()
+      }
+    }
+  }, [])
+
+  return <div id="popup-rating-widget"></div>
 }
+
+export default PopupRatingWidget
